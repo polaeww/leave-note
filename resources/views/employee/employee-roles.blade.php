016778a3650e2eebb91a2b84eb54c7a125ede535
Unknown <nuttaporn199600@gmail.com>
2018-01-16 23:48:26 +0700

    PDF
    test show PDF

--- resources/views/employee/employee-roles.blade.php
+++ resources/views/employee/employee-roles.blade.php
@@ -3,9 +3,10 @@
     <section class="wrapper">
         <br><h3><i class="fa fa-angle-right"></i> ระเบียบการลาของบริษัท</h3>
 
-        <!-- show pdf file -->
-        <embed width="600" height="800" name="plugin" src="PO.BIG 2017.017.pdf" type="application/pdf">
-            <!-- <object type="application/pdf" data="ใบสมัคร.pdf" width="700" height="500"></object> -->
+            <a href="https://www.scribd.com/document/369284616/Roles#from_embed">
 
+<!-- show pdf file -->
+        <!--<embed width="600" height="800" name="plugin" src="PO.BIG 2017.017.pdf" type="application/pdf">
+            <object type="application/pdf" data="ใบสมัคร.pdf" width="700" height="500"></object>-->
     </section>
     @endsection
