998418b232842db2b27e1412aa9cfc4fdfb5424e
Unknown <nuttaporn199600@gmail.com>
2018-01-17 10:03:06 +0700

    แก้ PDF
    อันเก่าเปิดไมม่ได้

--- resources/views/employee/employee-roles.blade.php
+++ resources/views/employee/employee-roles.blade.php
@@ -3,7 +3,9 @@
     <section class="wrapper">
         <br><h3><i class="fa fa-angle-right"></i> ระเบียบการลาของบริษัท</h3>
 
-            <a href="https://www.scribd.com/document/369284616/Roles#from_embed">
+        <p  style=" margin: 12px auto 6px auto; font-family: Helvetica,Arial,Sans-serif;  font-size: 14px;  display: block;"></p>
+        <iframe class="scribd_iframe_embed" src="https://www.scribd.com/embeds/369286183/content?start_page=1&view_mode=scroll&access_key=key-3OmpeEWlslPraRCvUJ7g&show_recommendations=true"
+                data-auto-height="false" data-aspect-ratio="0.7068965517241379" scrolling="no" id="doc_91013" width="100%" height="600" frameborder="0"></iframe>
 
 <!-- show pdf file -->
         <!--<embed width="600" height="800" name="plugin" src="PO.BIG 2017.017.pdf" type="application/pdf">
