9ad331c2e9eedce216feddd3553d1532f9a1b068
nipster4 <nuttaporn199600@gmail.com>
2018-02-12 01:53:39 +0700

    แก้ PDF

--- resources/views/employee/employee-roles.blade.php
+++ resources/views/employee/employee-roles.blade.php
@@ -2,9 +2,9 @@
 @section('content')
     <section class="wrapper">
         <br><h3><i class="fa fa-angle-right"></i> ระเบียบการลาของบริษัท</h3>
-
+        <iframe src="https://drive.google.com/file/d/19-7AsSH9DMnurWDWHELzNxaO_WNy1-fs/preview" width="100%" height="800"></iframe>
         <!-- show pdf file -->
-        <embed width="600" height="800" name="plugin" src="PO.BIG 2017.017.pdf" type="application/pdf">
+        {{--<embed width="600" height="800" name="plugin" src="PO.BIG 2017.017.pdf" type="application/pdf">--}}
             <!-- <object type="application/pdf" data="ใบสมัคร.pdf" width="700" height="500"></object> -->
 
     </section>
